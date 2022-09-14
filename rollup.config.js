7e48b1db5265998f0123df11214f9b12fac05bfa
Simon Reinisch <simon@reinisch.io>
2022-09-14 17:23:11 +0200

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json';
+
+export default {
+    input: 'src/index.ts',
+    plugins: [
+        typescript()
+    ],
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs'
+        },
+        {
+            file: pkg.module,
+            format: 'esm'
+        }
+    ]
+};
