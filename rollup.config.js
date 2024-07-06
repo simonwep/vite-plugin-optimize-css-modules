d95dda22bb304535b9449eecbf9b19f1784b766e
Simon Reinisch <simon@reinisch.io>
2024-07-06 15:26:29 +0200

    Update dependencies and add editorconfig

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
 import typescript from '@rollup/plugin-typescript';
-import pkg from './package.json' assert { type: 'json' };
+import pkg from './package.json' assert {type: 'json'};
 
 export default {
-    input: 'src/index.ts',
-    plugins: [
-        typescript()
-    ],
-    output: [
-        {
-            file: pkg.main,
-            format: 'cjs'
-        },
-        {
-            file: pkg.module,
-            format: 'esm'
-        }
-    ]
+  input: 'src/index.ts',
+  plugins: [
+    typescript()
+  ],
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'esm'
+    }
+  ]
 };
